0eded4b9773c69e0c4aec7fbe3769f3442dab611
JiaDingYi <wzy2010416033@163.com>
2019-12-17 14:14:42 +0800

    create  rewarded video controller

---
+++ PlayableAdMobDemo/RewardedVideoController.m
@@ -0,0 +1,13 @@
+//
+//  RewardedVideoController.m
+//  PlayableAdMobDemo
+//
+//  Created by 王泽永 on 2019/12/17.
+//  Copyright © 2019 playable. All rights reserved.
+//
+
+#import "RewardedVideoController.h"
+
+@implementation RewardedVideoController
+
+@end
