286dd67db1082ef2e95d46c510a6f320865e584f
JiaDingYi <wzy2010416033@163.com>
2019-12-17 11:57:02 +0800

    build atmosplay ads rewarded video adapter logic

--- PlayableAdMobDemo/Adapters/AtmosplayAdsRewardedVideoAdapter.h
+++ PlayableAdMobDemo/Adapters/AtmosplayAdsRewardedVideoAdapter.h
@@ -8,8 +8,8 @@
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
-
-@interface AtmosplayAdsRewardedVideoAdapter : NSObject
+@import GoogleMobileAds;
+@interface AtmosplayAdsRewardedVideoAdapter : NSObject <GADMediationAdapter>
 
 @end
 
