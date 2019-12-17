b4229debb9470b2af1b3521ce72ab70451ce70c0
JiaDingYi <wzy2010416033@163.com>
2019-12-17 11:04:23 +0800

    create atmosplay ads rewarded video adapter

---
+++ PlayableAdMobDemo/Adapters/AtmosplayAdsRewardedVideoAdapter.m
@@ -0,0 +1,12 @@
+//
+//  AtmosplayAdsRewardedVideoAdapter.m
+//  GoogleMobileAdsMediationAtmosplayAds
+//
+//  Created by 王泽永 on 2019/12/17.
+//
+
+#import "AtmosplayAdsRewardedVideoAdapter.h"
+
+@implementation AtmosplayAdsRewardedVideoAdapter
+
+@end
